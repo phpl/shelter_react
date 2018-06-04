27a6f666ec74cc1ccfde7fcfe576510e4b09e7c5
Pawel Hadam <pawel.hadam19@gmail.com>
2018-06-04 23:12:08 +0200

    Add navbar, home, about components with routing

---
+++ src/components/About.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+import { Jumbotron, Grid, Row, Col, Image, Button } from 'react-bootstrap';
+import './About.css';
+
+export default class About extends Component {
+  render() {
+    return (
+      <Grid>
+        <Jumbotron>
+          <h2>TEST</h2>
+        </Jumbotron>
+      </Grid>
+    )
+  }
+}
